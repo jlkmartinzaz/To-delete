7c023c1b68b50e0a01e0c389b017daad5ed0d76b
AndresMiranda <61157532+afmirandad@users.noreply.github.com>
2025-08-30 00:06:13 +0000

    Main: Initial commit

---
+++ controllers/band_controller.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+band_bp =Blueprint('band_bp',__name__)
+
+@band_bp.route('/bands', methods=['GET'])
+
+@band_bp.route('/bands/<int:band_id>', methods=['GET'])
+
+@band_bp.route('/bands', methods=['POST'])
+
+@band_bp.route('/bands/<int:band_id>', methods=['PUT'])
+
+@band_bp.route('/bands/<int:band_id>', methods=['DELETE'])
